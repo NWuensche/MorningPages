d37bf48dcb3e134a9d3b2eff12dbda216a8366aa
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-03-12 12:00:36 +0100

    Setup Espresso Tests

---
+++ app/src/androidTest/java/morningpages/app/niklas/morningpages/StartUpTests.kt
@@ -0,0 +1,9 @@
+package morningpages.app.niklas.morningpages
+
+/**
+ * Created by nwuensche on 12.03.17.
+ */
+class StartUpTests : SuperEspresso() {
+
+
+}
---
+++ app/src/androidTest/java/morningpages/app/niklas/morningpages/SuperEspresso.kt
@@ -0,0 +1,17 @@
+package morningpages.app.niklas.morningpages
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+/**
+ * Created by nwuensche on 12.03.17.
+ */
+@RunWith(AndroidJUnit4::class)
+abstract class SuperEspresso {
+    @Rule @JvmField val activity = ActivityTestRule<MainActivity>(MainActivity::class.java)
+    val appContext: Context = InstrumentationRegistry.getTargetContext()
+}
