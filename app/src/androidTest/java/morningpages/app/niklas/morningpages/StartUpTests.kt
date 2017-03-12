bf6da1d83cf054abfe0eafffa13e0081d0396ce9
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-03-12 13:53:24 +0100

    Look that buttons are invisible

--- app/src/androidTest/java/morningpages/app/niklas/morningpages/StartUpTests.kt
+++ app/src/androidTest/java/morningpages/app/niklas/morningpages/StartUpTests.kt
@@ -1,6 +1,11 @@
 package morningpages.app.niklas.morningpages
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.not
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
@@ -12,6 +17,9 @@ class StartUpTests : SuperEspresso() {
     @Test
     fun startRight() {
         assertThat(CurrTime.getCurrentTime(activity.activity), CoreMatchers.`is`("00:00:00"))
+
+        onView(withId(R.id.startTimer)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.deleteLastNumber)).check(matches(not(isDisplayed())))
     }
 
 }
