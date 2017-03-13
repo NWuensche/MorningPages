0e8cab585436f34227282610894c8b7645d8d277
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-03-13 20:56:09 +0100

    Somehow timer, does not work.

--- app/src/main/java/morningpages/app/niklas/morningpages/WriteActivity.kt
+++ app/src/main/java/morningpages/app/niklas/morningpages/WriteActivity.kt
@@ -2,8 +2,14 @@ package morningpages.app.niklas.morningpages
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.view.menu.ActionMenuItemView
+import android.util.Log
 import android.view.Menu
-import java.util.*
+import rx.Observable
+import rx.lang.kotlin.subscribeBy
+import rx.schedulers.Schedulers
+import rx.subjects.PublishSubject
+import rx.subjects.ReplaySubject
 
 class WriteActivity : AppCompatActivity() {
 
@@ -14,8 +20,17 @@ class WriteActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_write, menu)
-        menu!!.findItem(R.id.show_timer).title = intent.extras.getString("time")
         return super.onCreateOptionsMenu(menu)
     }
 
+    //TODO First time, not other times.
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        val item = findViewById(R.id.show_timer) as ActionMenuItemView
+        //val times = CurrTime.formatTimeAndCreateList(intent.extras.getString("time"))
+        PublishSubject.interval(1, java.util.concurrent.TimeUnit.SECONDS, Schedulers.newThread())
+                .subscribeBy(onNext = {item.text = it.toString()})
+
+        super.onWindowFocusChanged(hasFocus)
+
+    }
 }
