cdf54261d7e0eea0eabc33e49442b24d7ba280e3
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-03-13 16:53:52 +0100

    Delete Action bar false example

--- app/src/main/java/morningpages/app/niklas/morningpages/WriteActivity.kt
+++ app/src/main/java/morningpages/app/niklas/morningpages/WriteActivity.kt
@@ -3,19 +3,17 @@ package morningpages.app.niklas.morningpages
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import kotlinx.android.synthetic.main.write_action_bar.*
 
 class WriteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
-        supportActionBar!!.customView = textView
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_write, menu)
         return super.onCreateOptionsMenu(menu)
     }
-    
+
 }
