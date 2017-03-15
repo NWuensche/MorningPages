be14aa305f28515413fc2c524e1aa4e854bd37a2
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-03-15 19:04:57 +0100

    Add test to look if second attempt works

--- app/src/androidTest/java/morningpages/app/niklas/morningpages/FinishedTests.kt
+++ app/src/androidTest/java/morningpages/app/niklas/morningpages/FinishedTests.kt
@@ -1,8 +1,7 @@
 package morningpages.app.niklas.morningpages
 
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.action.ViewActions.pressBack
+import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import org.junit.Test
@@ -29,8 +28,28 @@ class FinishedTests : SuperEspresso() {
         onView(isRoot()).perform(pressBack())
         onView(withId(R.id.text1)).check(matches(isDisplayed())) // Back at Timer
         //TODO onResume mit pressMenu und so testen, wenn geht dann auch in 5MJ
+    }
 
+    @Test
+    fun isOldTextGoneAndDoesSecondWriteWork() {
+        onView(withId(R.id.text5)).perform(click())
+        onView(withId(R.id.startTimer)).perform(click())
 
+        onView(withId(R.id.writeText)).perform(typeText("Test"))
+        onView(withId(R.id.writeText)).check(matches(withText(("Test"))))
+
+        Thread.sleep(5000)
+
+        onView(isRoot()).perform(pressBack())
+
+        onView(withId(R.id.text2)).perform(click())
+        onView(withId(R.id.startTimer)).perform(click())
+        Thread.sleep(500)
+
+        onView(withId(R.id.writeText)).check(matches(withText((""))))
+        Thread.sleep(2000)
+
+        onView(withId(R.id.monkeyCage)).check(matches(isDisplayed()))
     }
 
     fun switchToActivity() {
